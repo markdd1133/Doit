0d9e73a625ac4dc5581da2d6779c84355d115af1
markdd1133 <shengchichen@Shengde-MacBook-Air.local>
2017-05-02 17:47:59 +0800

    changed viewController

---
+++ Doit/CreateTaskViewController.swift
@@ -0,0 +1,46 @@
+//
+//  CreateTaskViewController.swift
+//  Doit
+//
+//  Created by Sheng Chi Chen on 2017/5/2.
+//  Copyright © 2017年 Sheng Chi Chen. All rights reserved.
+//
+
+import UIKit
+
+class CreateTaskViewController: UIViewController {
+
+    @IBOutlet weak var taskNameTextField: UITextField!
+    @IBOutlet weak var importantSwitch: UISwitch!
+    @IBAction func addTapped(_ sender: Any) {
+        //Create a Task from the outlet information
+        
+        let task = Task()
+        task.name = taskNameTextField.text!
+        task.important = importantSwitch.isOn
+        
+        
+        //Add new task to array in previous viewController
+        previousVC.tasks.append(task)
+        previousVC.tableView.reloadData()
+        navigationController?.popViewController(animated: true)
+    }
+    
+    var previousVC = TasksViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Doit/TasksViewController.swift
@@ -0,0 +1,68 @@
+//
+//  TasksViewController.swift
+//  Doit
+//
+//  Created by Sheng Chi Chen on 2017/5/2.
+//  Copyright © 2017年 Sheng Chi Chen. All rights reserved..
+//
+
+import UIKit
+
+class TasksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    var tasks:[Task] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tasks = makeTasks()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        if task.important{
+            cell.textLabel?.text = "❗️\(task.name)"
+        }else{
+            cell.textLabel?.text = task.name
+        }
+        return cell
+    }
+    
+    func makeTasks() -> [Task]{
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Buy Cheese"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Mow the lawn"
+        task3.important = false
+        
+        return [task1, task2, task3]
+    }
+    
+    @IBAction func plusTapped(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let nextVC = segue.destination as! CreateTaskViewController
+        nextVC.previousVC = self
+    }
+}
+
